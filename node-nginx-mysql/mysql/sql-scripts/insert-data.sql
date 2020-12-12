c739f51079efc0f0dd777624af6fd6a0030af504
Lucas Vasconcelos <lucas@vscn.co>
2020-12-12 02:49:17 -0300

    app funcionalidade e visual satisfatorios

--- node-nginx-mysql/mysql/sql-scripts/insert-data.sql
+++ node-nginx-mysql/mysql/sql-scripts/insert-data.sql
@@ -1,4 +1,10 @@
-INSERT INTO `people` (`name`) VALUES ('Case');
-INSERT INTO `people` (`name`) VALUES ('Trinity');
-INSERT INTO `people` (`name`) VALUES ('Ratz');
-INSERT INTO `people` (`name`) VALUES ('Wage');
+INSERT INTO `people` (`name`) VALUES ('Light Yagami');
+INSERT INTO `people` (`name`) VALUES ('Kiyomi Takada');
+INSERT INTO `people` (`name`) VALUES ('Rem');
+INSERT INTO `people` (`name`) VALUES ('Watari');
+INSERT INTO `people` (`name`) VALUES ('Matt');
+INSERT INTO `people` (`name`) VALUES ('Mello');
+INSERT INTO `people` (`name`) VALUES ('Raye Penber');
+INSERT INTO `people` (`name`) VALUES ('Soichiro Yagami');
+INSERT INTO `people` (`name`) VALUES ('Naomi Misora');
+INSERT INTO `people` (`name`) VALUES ('L');
